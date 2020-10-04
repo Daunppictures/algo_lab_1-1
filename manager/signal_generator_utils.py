0638ecd088c5fa73246aae3bece398bd5801cebc
Raven <redstoyningbros@gmail.com>
2020-10-05 00:52:11 +0300

    Created model and manager for Signal Generator

---
+++ manager/signal_generator_utils.py
@@ -0,0 +1,10 @@
+from typing import List
+from model.signal_generator import SignalGenerator
+
+
+def sort_by_memory_capacity_desc(signal_generators: List[SignalGenerator]) -> List[SignalGenerator]:
+    pass
+
+
+def sort_by_signal_frequency_asc(signal_generators: List[SignalGenerator]) -> List[SignalGenerator]:
+    pass
