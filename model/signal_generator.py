2f8d2de36f9a125a61f59380e6c08451e92efe57
Raven <redstoyningbros@gmail.com>
2020-10-05 13:51:23 +0300

    Added __str__implementation

--- model/signal_generator.py
+++ model/signal_generator.py
@@ -1,6 +1,10 @@
 class SignalGenerator(object):
-    
+
     def __init__(self, max_signal_frequency_in_megahz: int, memory_capacity_in_kb: int, brand: str):
         self.max_signal_frequency_in_megahz = max_signal_frequency_in_megahz
         self.memory_capacity_in_kb = memory_capacity_in_kb
         self.brand = brand
+
+    def __str__(self) -> str:
+        return f"SignalGenerator: [max_signal_frequency_in_megahz: {self.max_signal_frequency_in_megahz}, " \
+               f"memory_capacity_in_kb: {self.memory_capacity_in_kb}, brand: {self.brand}]"
